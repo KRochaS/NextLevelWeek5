2f13b7cd17a9b29e99fa263ee26921eacff8fc3f
KRochaS <karinerocha.s@hotmail.com>
2021-04-21 22:23:06 -0300

    get Episodes

--- trilha-reactjs/src/pages/index.tsx
+++ trilha-reactjs/src/pages/index.tsx
@@ -1,10 +1,21 @@
-// SPA
-// SSR
-// SSG
+import {GetStaticProps } from 'next';
+import { api } from '../services/api';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+// import { useEffect } from "react"
 
-import { useEffect } from "react"
+type Episode = {
+    id: string,
+    title: string;
+    members: string;
+    published_at: string;
+}
+type HomeProps = {
+    episodes:  Episode[],
+}
 
-export default function Home(props) {
+export default function Home(props: HomeProps) {
 
     console.log(props.episodes);
 
@@ -20,7 +31,10 @@ export default function Home(props) {
 
 
     return (
-        <h1> Index </h1>
+        <div>
+            <h1> Index </h1>
+            <p> { JSON.stringify(props.episodes)}</p>
+        </div>
     )
 }
 
@@ -43,15 +57,32 @@ export default function Home(props) {
 // SSG
 // Cria uma versão estática da página (HTML puro)
 // é servido para todos os usuários depois do primeiro usuário acessar
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
 
-    const response = await fetch('http://localhost:3333/episodes')
-    const data = await response.json();
-    console.log(data);
+    const { data } =  await api.get('episodes', {
+        params: {
+            _limit: 12,
+            _sort: 'published_at',
+            _order: 'desc',
+        }
+    })
 
+    const episodes = data.map(episode => {
+        return {
+            id: episode.id,
+            title: episode.title,
+            thumbnail: episode.thumbnail,
+            publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
+            duration: Number(episode.file.duration),
+            durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+            description: episode.description,
+            url: episode.file.url,
+        }
+    })
+  
     return {
         props: {
-            episodes: data
+            episodes
         },
         // de quanto em quanto tempo
         // será gerada uma nova versão da página
