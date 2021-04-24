2f8f58c88af3fcd03a6cd5e9d83490c1d6563860
KRochaS <karinerocha.s@hotmail.com>
2021-04-24 01:49:44 -0300

    [slug]

--- trilha-reactjs/src/pages/episodes/[slug].tsx
+++ trilha-reactjs/src/pages/episodes/[slug].tsx
@@ -1,9 +1,98 @@
+import { format, parseISO, } from 'date-fns';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import { useRouter } from 'next/router';
+import { api } from '../../services/api';
+import ptBR from 'date-fns/locale/pt-BR';
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
+import styles from './episode.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export default function Episode() {
-    const router = useRouter();
+type Episode = {
+    id: string,
+    title: string,
+    thumbnail: string,
+    description: string,
+    members: string,
+    duration: string,
+    url: string,
+    publishedAt: string,
+    durationAsString: string,
+}
+type EpisodeProps = {
+    episode: Episode
+}
+
+export default function Episode({ episode }: EpisodeProps) {
+
 
     return (
-        <h1> {router.query.slug} </h1>
+        <div className={styles.episode}>
+            <div className={styles.thumbnailContainer}>
+                <Link href={`/`}>
+                <button type="button">
+                    <img src="/arrow-left.svg" alt="Voltar" />
+                </button>
+                
+                </Link>
+                <Image width={700}
+                    height={160}
+                    src={episode.thumbnail}
+                    objectFit="cover" />
+                <button type="button">
+                    <img src="/play.svg" alt="Tocar episódio"/>
+                </button>
+            </div>
+
+            <header>
+                <h1>{episode.title}</h1>
+                <span> {episode.members}</span>
+                <span> {episode.publishedAt}</span>
+                <span> {episode.durationAsString}</span>
+            </header>
+
+
+            {/* 
+                dangerouslySetInnerHTML = seta os dados do back  que vem já
+                com a tag p em  HTML
+
+            */}
+            <div className={styles.description} dangerouslySetInnerHTML={ {__html: episode.description}}/>
+               
+        
+        </div>
     )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [],
+        fallback: 'blocking'
+    }
+}
+
+// ctx = contexto;
+export const getStaticProps: GetStaticProps = async (ctx) => {
+    // slug do arquivo [slug] - mesmo nome;
+
+    const { slug } = ctx.params;
+    const { data } = await api.get(`/episodes/${slug}`)
+
+    const episode = {
+        id: data.id,
+        title: data.title,
+        thumbnail: data.thumbnail,
+        publishedAt: format(parseISO(data.published_at), 'd MMM yy', { locale: ptBR }),
+        duration: Number(data.file.duration),
+        durationAsString: convertDurationToTimeString(Number(data.file.duration)),
+        description: data.description,
+        url: data.file.url,
+        members: data.members,
+    };
+    return {
+        props: {
+            episode
+        },
+        revalidate: 60 * 60 * 24, // 24 hours
+    }
 }
