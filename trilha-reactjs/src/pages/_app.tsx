e284a7c185cdcd0093e7eaa6dc964ac69f6ca83e
KRochaS <karinerocha.s@hotmail.com>
2021-04-26 20:20:01 -0300

    _app.tsx

--- trilha-reactjs/src/pages/_app.tsx
+++ trilha-reactjs/src/pages/_app.tsx
@@ -3,46 +3,25 @@ import '../styles/global.scss';
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
 import styles from '../styles/app.module.scss';
-import {PlayerContext } from '../contexts/PlayerContext';
-import { useState } from 'react';
+import { PlayerContextProvider } from '../contexts/PlayerContext';
 
 function MyApp({ Component, pageProps }) {
 
-    const [episodeList, setEpisodeList] = useState([]);
-    const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-    const [isPlaying, setIsPlaying] = useState(false);
-
-
-    function play(episode) {
-        setEpisodeList([episode]);
-        setCurrentEpisodeIndex(0);
-        setIsPlaying(true);
-    }
-
-    function tooglePlay() {
-
-        setIsPlaying(!isPlaying);
-
-    }
-
-    function setPlayingState(state) {
-        setIsPlaying(state);
-    }
-
-
     return (
-        <PlayerContext.Provider value={ {episodeList, currentEpisodeIndex, isPlaying, play, tooglePlay, setPlayingState}}>
-        <div className={styles.wrapper}>
 
-            <main>
-                <Header />
-                <Component {...pageProps} />
-            </main>
+        <PlayerContextProvider>
+            <div className={styles.wrapper}>
 
-            <Player />
-        </div>
+                <main>
+                    <Header />
+                    <Component {...pageProps} />
+                </main>
 
-        </PlayerContext.Provider>
+                <Player />
+            </div>
+        </PlayerContextProvider>
+
+
     );
 }
 
