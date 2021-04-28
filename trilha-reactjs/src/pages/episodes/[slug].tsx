28340ad6208be1b7561dcb750f8bc88779d69b3f
KRochaS <karinerocha.s@hotmail.com>
2021-04-28 19:37:22 -0300

    slug

--- trilha-reactjs/src/pages/episodes/[slug].tsx
+++ trilha-reactjs/src/pages/episodes/[slug].tsx
@@ -3,11 +3,12 @@ import ptBR from 'date-fns/locale/pt-BR';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useContext } from 'react';
 
+import { usePlayer } from '../../contexts/PlayerContext';
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 import styles from './episode.module.scss';
-import {useRouter } from 'next/router';
 
 type Episode = {
     id: string,
@@ -21,11 +22,14 @@ type Episode = {
     durationAsString: string,
 }
 type EpisodeProps = {
-    episode: Episode
+    episode: Episode,
 }
 
 export default function Episode({ episode }: EpisodeProps) {
     
+    const { play } = usePlayer();
+
+
     return (
         <div className={styles.episode}>
             <div className={styles.thumbnailContainer}>
@@ -39,7 +43,7 @@ export default function Episode({ episode }: EpisodeProps) {
                     height={160}
                     src={episode.thumbnail}
                     objectFit="cover" />
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                     <img src="/play.svg" alt="Tocar episódio"/>
                 </button>
             </div>
@@ -100,7 +104,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
 // ctx = contexto;
 export const getStaticProps: GetStaticProps = async (ctx) => {
     // slug do arquivo [slug] - mesmo nome;
-
     const { slug } = ctx.params;
     const { data } = await api.get(`/episodes/${slug}`)
 
