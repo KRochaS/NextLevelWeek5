8d06cbdf30c54cb35fe50dc7dc4e36a05178013e
KRochaS <karinerocha.s@hotmail.com>
2021-05-01 17:19:54 -0300

    _app.tsx

--- trilha-reactjs/src/pages/_app.tsx
+++ trilha-reactjs/src/pages/_app.tsx
@@ -1,25 +1,31 @@
 import '../styles/global.scss';
 
+import React from 'react';
+
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
-import styles from '../styles/app.module.scss';
 import { PlayerContextProvider } from '../contexts/PlayerContext';
+import { ThemeProvider } from '../contexts/ThemeContext';
+import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
 
     return (
 
-        <PlayerContextProvider>
-            <div className={styles.wrapper}>
 
-                <main>
-                    <Header />
-                    <Component {...pageProps} />
-                </main>
+        <ThemeProvider>
+            <PlayerContextProvider>
+                <div className={styles.wrapper}>
 
-                <Player />
-            </div>
-        </PlayerContextProvider>
+                    <main>
+                        <Header />
+                        <Component {...pageProps} />
+                    </main>
+
+                    <Player />
+                </div>
+            </PlayerContextProvider>
+        </ThemeProvider>
 
 
     );
