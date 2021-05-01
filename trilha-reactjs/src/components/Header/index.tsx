c917a2eb71c5e0ff618cb53041cf9c5e9ea919df
KRochaS <karinerocha.s@hotmail.com>
2021-05-01 17:18:36 -0300

    ThemeContext

--- trilha-reactjs/src/components/Header/index.tsx
+++ trilha-reactjs/src/components/Header/index.tsx
@@ -1,21 +1,35 @@
-import  styles from './styles.module.scss';
 import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useContext } from 'react';
+
+import { ThemeContext } from '../../contexts/ThemeContext';
+import styles from './styles.module.scss';
 
 export function Header() {
+
+    const { isDarkMode, darkMode } = useContext(ThemeContext);
 
     const currentDate = format(new Date(), 'EEEEEE, d, MMM', {
 		locale: ptBR,
 	});
 
+    function handleIsThemeDark() {
+        darkMode();
+    }
+
 
     return (
         <header className={styles.headerContainer}>
-            <img src="/logo.svg" alt="Podcastr"/>
+            <img src={!isDarkMode ? `/logo.svg` : `/logo-dark.svg`} alt="Podcastr"/>
 
             <p> O melhor para você ouvir, sempre </p>
 
             <span> {currentDate} </span>
+
+             
+            <button type="button" onClick={handleIsThemeDark}>
+                <img src={!isDarkMode ? `/moon.svg` : `/sun.svg`} alt="moonOrSun" />
+            </button>
         </header>
     );
 }
