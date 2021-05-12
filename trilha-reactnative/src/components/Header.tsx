98c65f93346cd83042ed92d4bcd5fa94a20f0130
KRochaS <karinerocha.s@hotmail.com>
2021-05-11 21:56:46 -0300

    Header

---
+++ trilha-reactnative/src/components/Header.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+    StyleSheet, 
+    Text, 
+    View, 
+    Image
+} from 'react-native';
+import colors from '../styles/colors';
+
+import userImg from '../assets/profile.png';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import fonts from '../styles/fonts';
+
+export function Header() {
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.greeting}> Olá </Text>
+                <Text style={styles.userName}> Karine </Text>
+            </View>
+
+            <Image source={userImg} style={styles.image}/>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 20,
+        marginTop: getStatusBarHeight(),
+        padding: 20
+    },
+    image: {
+        width: 100,
+        height: 100,
+        borderRadius: 50
+    },
+    greeting: {
+        fontSize: 32,
+        color: colors.heading,
+        fontFamily: fonts.text,
+    },
+    userName: {
+        fontSize: 32,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        lineHeight: 40
+    },
+})
