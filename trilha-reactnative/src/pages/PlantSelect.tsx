3d4a1729901cb73e4ad7c08e745f919d701b2676
KRochaS <karinerocha.s@hotmail.com>
2021-05-12 20:02:33 -0300

    PlantSelect

--- trilha-reactnative/src/pages/PlantSelect.tsx
+++ trilha-reactnative/src/pages/PlantSelect.tsx
@@ -4,12 +4,23 @@ import { View, Text, StyleSheet } from 'react-native';
 import colors from '../styles/colors';
 
 import { Header } from '../components/Header'
+import fonts from '../styles/fonts';
 
 
 export function PlantSelect() {
     return (
         <View style={styles.container}>
+            <View style={styles.header}>
                 <Header />
+
+                <Text style={styles.title}>
+                    Em qual ambiente
+                </Text>
+                <Text style={styles.subtitle}>
+                    você quer colocar sua planta?
+                </Text>
+
+            </View>
         </View>
     )
 }
@@ -19,4 +30,20 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: colors.background
     },
+    header: {
+        paddingHorizontal: 30
+    },
+    title: {
+        fontSize: 17,
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        lineHeight: 20,
+        marginTop: 15
+    },
+    subtitle: {
+        fontFamily: fonts.text,
+        fontSize: 17,
+        lineHeight: 20,
+        colors: colors.heading
+    }
 });
