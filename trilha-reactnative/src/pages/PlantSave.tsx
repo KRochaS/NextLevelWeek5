7f7705004f85685f9b31de568e0809c324bbb231
KRochaS <karinerocha.s@hotmail.com>
2021-05-18 22:09:20 -0300

    PlantSave

--- trilha-reactnative/src/pages/PlantSave.tsx
+++ trilha-reactnative/src/pages/PlantSave.tsx
@@ -1,39 +1,20 @@
-import React, { useState } from 'react';
-
-import {
-    Alert,
-    StyleSheet,
-    View,
-    Text,
-    Image,
-    ScrollView,
-    Platform,
-    TouchableOpacity
-} from 'react-native';
-import { getBottomSpace } from 'react-native-iphone-x-helper';
-import { useRoute } from '@react-navigation/core';
-import { SvgFromUri } from 'react-native-svg';
-import waterdrop from '../assets/waterdrop.png';
-import { Button } from '../components/Button';
-import colors from '../styles/colors';
-import fonts from '../styles/fonts';
 import DateTimerPicker, { Event } from '@react-native-community/datetimepicker';
+import { useRoute } from '@react-navigation/core';
 import { isBefore } from 'date-fns';
 import { format } from 'date-fns/esm';
+import React, { useEffect, useState } from 'react';
+import { Alert, Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+import { SvgFromUri } from 'react-native-svg';
+
+import waterdrop from '../assets/waterdrop.png';
+import { Button } from '../components/Button';
+import { loadPlant, PlantProps, savePlant } from '../libs/storage';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 interface Params {
-    plant: {
-        id: string;
-        name: string;
-        about: string;
-        water_tips: string;
-        photo: string;
-        environments: [string];
-        frequency: {
-            times: number;
-            repeat_every: string;
-        }
-    }
+    plant: PlantProps
 }
 
 export function PlantSave() {
@@ -61,6 +42,20 @@ export function PlantSave() {
 
     function handleOpenDateTimePickerForAndroid() {
         setShowDatePicker(oldState => !oldState);
+    }
+
+
+    async function handleSave() {
+        try {
+            await savePlant({
+                ...plant,
+                dateTimeNotification: selectedDateTime,
+            });
+
+
+        } catch {
+            Alert.alert('Não foi possível salvar. 😥');
+        }
     }
 
 
@@ -120,7 +115,7 @@ export function PlantSave() {
 
                 <Button
                     title="Cadastrar planta"
-                    onPress={() => { }}
+                    onPress={handleSave}
                 />
             </View>
         </View>
