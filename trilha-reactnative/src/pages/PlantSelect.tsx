2e953613c503e6ecc56d063357be92060f6f054a
KRochaS <karinerocha.s@hotmail.com>
2021-05-18 22:09:26 -0300

    PlantSelect

--- trilha-reactnative/src/pages/PlantSelect.tsx
+++ trilha-reactnative/src/pages/PlantSelect.tsx
@@ -6,6 +6,7 @@ import { EnviromentButton } from '../components/EnviromentButton';
 import { Header } from '../components/Header';
 import { Load } from '../components/Load';
 import { PlantCardPrimary } from '../components/PlantCardPrimary';
+import { PlantProps } from '../libs/storage';
 import api from '../services/api';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
@@ -17,18 +18,6 @@ interface EnviromentProps {
     title: string;
 }
 
-interface PlantProps {
-    id: string;
-    name: string;
-    about: string;
-    water_tips: string;
-    photo: string;
-    environments: [string];
-    frequency: {
-        times: number;
-        repeat_every: string;
-    }
-}
 
 export function PlantSelect() {
     const [enviroments, setEnvironment] = useState<EnviromentProps[]>([]);
