7a7f45253a35d01f64afa849cd4974338daa7958
KRochaS <karinerocha.s@hotmail.com>
2021-05-20 20:10:10 -0300

    MyPlants.tsx

---
+++ trilha-reactnative/src/pages/MyPlants.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image
+} from 'react-native';
+import { Header } from '../components/Header';
+
+export function MyPlants() {
+    return (
+        <View style={styles.container}> 
+            <Header />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        
+    },
+
+})
