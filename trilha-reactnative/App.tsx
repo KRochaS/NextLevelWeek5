d6cb1372672785d2c5ccc265fe2c14d8183df483
KRochaS <karinerocha.s@hotmail.com>
2021-05-24 19:42:49 -0300

    App

--- trilha-reactnative/App.tsx
+++ trilha-reactnative/App.tsx
@@ -1,8 +1,12 @@
 import { Jost_400Regular, Jost_600SemiBold, useFonts } from '@expo-google-fonts/jost';
 import AppLoading from 'expo-app-loading';
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as Notifications from 'expo-notifications';
+
+
 
 import Routes from './src/routes';
+import { PlantProps } from './src/libs/storage';
 
 
 export default function App() {
@@ -11,6 +15,29 @@ export default function App() {
         Jost_400Regular,
         Jost_600SemiBold,
     });
+
+    // useEffect(() => {
+        // const subscription = Notifications.addNotificationReceivedListener(
+        //     async notification => {
+        //         const data = notification.request.content.data.plant as PlantProps;
+        //     }
+        // )
+
+        // return () => subscription.remove();
+
+        // Notificações Agendas
+    //     async function notifications() {
+    //     //    const data = await Notifications.getAllScheduledNotificationsAsync();
+    //     //     console.log(data);
+
+    //     // Cancelar todas as notificações
+    //     await Notifications.cancelAllScheduledNotificationsAsync();
+    //     }
+
+    //     notifications();
+
+
+    // }, [])
 
 
     if (!fontsLoaded)
